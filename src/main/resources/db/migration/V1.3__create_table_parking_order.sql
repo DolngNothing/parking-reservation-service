b9a023fd59a3900a00dac3237ef98f35aac3954f
ZHOUGR2 <286348794@qq.com>
2020-08-10 13:57:11 +0800

    [init] [Green] feature: add create table parking order sql

---
+++ src/main/resources/db/migration/V1.3__create_table_parking_order.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `parking_order`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `parking_lot_id` int(11) NOT NULL,
+  `parking_start_time` datetime(0) NOT NULL,
+  `parking_end_time` datetime(0) NOT NULL,
+  `fetch_number` int(11) DEFAULT NULL,
+  `create_time` datetime(0) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `status` int(11) DEFAULT NULL,
+  `car_number` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Fixed;
