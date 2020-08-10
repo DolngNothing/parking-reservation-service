714960c91026bda47ea75a57a910d6cd0915e1c2
alex <alex.h.z.xu@oocl.com>
2020-08-10 17:11:09 +0800

    [alex] feature: add jacoco

---
+++ src/test/java/com/oocl/parkingreservationservice/resources/db/migration/V1.1__create_table_car.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `car`(
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `car_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
