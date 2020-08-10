9a0a2887fafc5d817608eb30236e611a0a71f68b
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-10 15:50:50 +0800

    [BookParking][James] feature:add should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_email_123 test

--- src/test/java/com/oocl/parkingreservationservice/service/ParkingOrdersServiceTest.java
+++ src/test/java/com/oocl/parkingreservationservice/service/ParkingOrdersServiceTest.java
@@ -3,20 +3,14 @@ package com.oocl.parkingreservationservice.service;
 import com.oocl.parkingreservationservice.constants.StatusContants;
 import com.oocl.parkingreservationservice.exception.IllegalParameterException;
 import com.oocl.parkingreservationservice.model.ParkingOrder;
-import com.oocl.parkingreservationservice.model.User;
 import com.oocl.parkingreservationservice.repository.ParkingOrderRepository;
 import com.oocl.parkingreservationservice.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.BDDMockito.given;
 
 public class ParkingOrdersServiceTest {
     private ParkingOrderService parkingOrderService;
@@ -35,10 +29,11 @@ public class ParkingOrdersServiceTest {
     void should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_phone_number_123() {
         //given
         String illegalPhone="123";
+        String email="1214852999@qq.com";
         ParkingOrder parkingOrder=new ParkingOrder(null,1,"2020-08-10","2020-8-11",null,1,null, StatusContants.WAIT_FOR_SURE,"浙A1063警");
 
         //when
-        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,illegalPhone));
+        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,illegalPhone, email));
 
         //then
         assertEquals(IllegalParameterException.class, exception.getClass());
@@ -49,13 +44,26 @@ public class ParkingOrdersServiceTest {
         //given
         String illegal_car_number="123";
         String phone="15920138477";
+        String email="1214852999@qq.com";
         ParkingOrder parkingOrder=new ParkingOrder(null,1,"2020-08-10","2020-8-11",null,1,null, StatusContants.WAIT_FOR_SURE,illegal_car_number);
 
         //when
-        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,phone));
+        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,phone, email));
 
         //then
         assertEquals(IllegalParameterException.class, exception.getClass());
     }
+    @Test
+    void should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_email_123() {
+        //given
+        String illegalEmail="123";
+        String phone="15920138477";
+        ParkingOrder parkingOrder=new ParkingOrder(null,1,"2020-08-10","2020-8-11",null,1,null, StatusContants.WAIT_FOR_SURE,"浙A1063警");
 
+        //when
+        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,phone,illegalEmail));
+
+        //then
+        assertEquals(IllegalParameterException.class, exception.getClass());
+    }
 }
