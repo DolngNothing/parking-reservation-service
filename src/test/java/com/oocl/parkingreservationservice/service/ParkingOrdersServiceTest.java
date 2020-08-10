db1d76c46ae7513d0201bdd2a14faf51c0a1c175
ZHOUGR2 <286348794@qq.com>
2020-08-10 18:02:55 +0800

    [Confirm order-backend] [Green] feature : add  test case should_throw_order_canceled_excption_when_confirm_order_given_confirmed_order_id and implement it

--- src/test/java/com/oocl/parkingreservationservice/service/ParkingOrdersServiceTest.java
+++ src/test/java/com/oocl/parkingreservationservice/service/ParkingOrdersServiceTest.java
@@ -22,11 +22,12 @@ public class ParkingOrdersServiceTest {
     private ParkingOrderService parkingOrderService;
     private ParkingOrderRepository parkingOrderRepository;
     private UserRepository userRepository;
+
     @BeforeEach
     public void init() {
         parkingOrderRepository = Mockito.mock(ParkingOrderRepository.class);
-        userRepository=Mockito.mock(UserRepository.class);
-        parkingOrderService=new ParkingOrderService(parkingOrderRepository);
+        userRepository = Mockito.mock(UserRepository.class);
+        parkingOrderService = new ParkingOrderService(parkingOrderRepository);
     }
 
     @Test
@@ -34,8 +35,8 @@ public class ParkingOrdersServiceTest {
 //        given
         Integer orderId = 1;
 
-        ParkingOrder parkingOrder = new ParkingOrder(orderId,1,"2020-8-10 12:25:30",
-                "2020-8-10 14:25:30",1,1,"2020-8-10 14:25:30", StatusContants.WAIT_FOR_SURE,"1234",10.0);
+        ParkingOrder parkingOrder = new ParkingOrder(orderId, 1, "2020-8-10 12:25:30",
+                "2020-8-10 14:25:30", 1, 1, "2020-8-10 14:25:30", StatusContants.WAIT_FOR_SURE, "1234", 10.0);
         ParkingOrderResponse parkingOrderResponse;
         ParkingOrderRepository parkingOrderRepository = mock(ParkingOrderRepository.class);
         given(parkingOrderRepository.findById(orderId)).willReturn(java.util.Optional.of(parkingOrder));
@@ -50,41 +51,55 @@ public class ParkingOrdersServiceTest {
     void should_throw_wrong_message_when_confirm_order_given_order_id() {
 //        given
         Integer orderId = 1;
-        ParkingOrder parkingOrder = new ParkingOrder(orderId,1,"2020-8-10 12:25:30",
-                "2020-8-10 14:25:30",1,1,"2020-8-10 14:25:30", StatusContants.ALREADY_SURE,"1234",10.0);
+        ParkingOrder parkingOrder = new ParkingOrder(orderId, 1, "2020-8-10 12:25:30",
+                "2020-8-10 14:25:30", 1, 1, "2020-8-10 14:25:30", StatusContants.DELETED, "1234", 10.0);
         ParkingOrderRepository parkingOrderRepository = mock(ParkingOrderRepository.class);
         given(parkingOrderRepository.findById(orderId)).willReturn(java.util.Optional.of(parkingOrder));
         ParkingOrderService parkingOrderService = new ParkingOrderService(parkingOrderRepository);
 //        when
         Exception exception = assertThrows(IllegalOrderOperationException.class, () -> parkingOrderService.confirmParkingOrder(orderId));
 //        then
-        assertEquals(MessageConstants.ODER_CONFIRMED,exception.getMessage());
+        assertEquals(MessageConstants.ODER_CANCELED, exception.getMessage());
     }
-    //Illegal order operation exception
+
+    @Test
+    void should_throw_order_canceled_excption_when_confirm_order_given_confirmed_order_id() {
+//        given
+        Integer orderId = 1;
+        ParkingOrder parkingOrder = new ParkingOrder(orderId, 1, "2020-8-10 12:25:30",
+                "2020-8-10 14:25:30", 1, 1, "2020-8-10 14:25:30", StatusContants.ALREADY_SURE, "1234", 10.0);
+        ParkingOrderRepository parkingOrderRepository = mock(ParkingOrderRepository.class);
+        given(parkingOrderRepository.findById(orderId)).willReturn(java.util.Optional.of(parkingOrder));
+        ParkingOrderService parkingOrderService = new ParkingOrderService(parkingOrderRepository);
+//        when
+        Exception exception = assertThrows(IllegalOrderOperationException.class, () -> parkingOrderService.confirmParkingOrder(orderId));
+//        then
+        assertEquals(MessageConstants.ODER_CONFIRMED, exception.getMessage());
+    }
 
     @Test
     void should_return_success_message_when_cancel_order_given_uncertain_order_id() {
         //given
         int orderId = 1;
-        ParkingOrder order = new ParkingOrder(orderId,1,"2020-8-10 12:25:30",
-                "2020-8-10 14:25:30",1,1,"2020-8-10 14:25:30", StatusContants.WAIT_FOR_SURE,"1234",10.0);
+        ParkingOrder order = new ParkingOrder(orderId, 1, "2020-8-10 12:25:30",
+                "2020-8-10 14:25:30", 1, 1, "2020-8-10 14:25:30", StatusContants.WAIT_FOR_SURE, "1234", 10.0);
         ParkingOrderRepository parkingOrderRepository = mock(ParkingOrderRepository.class);
         ParkingOrderService parkingOrderService = new ParkingOrderService(parkingOrderRepository);
         //when
-        parkingOrderRepository.updateStatus(StatusContants.WAIT_FOR_SURE,orderId);
+        parkingOrderRepository.updateStatus(StatusContants.WAIT_FOR_SURE, orderId);
         //then
-        Mockito.verify(parkingOrderRepository).updateStatus(StatusContants.WAIT_FOR_SURE,orderId);
+        Mockito.verify(parkingOrderRepository).updateStatus(StatusContants.WAIT_FOR_SURE, orderId);
     }
 
     @Test
     void should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_phone_number_123() {
         //given
-        String illegalPhone="123";
-        String email="1214852999@qq.com";
-        ParkingOrder parkingOrder=new ParkingOrder(null,1,"2020-08-10","2020-8-11",null,1,null, StatusContants.WAIT_FOR_SURE,"浙A1063警",10.0);
+        String illegalPhone = "123";
+        String email = "1214852999@qq.com";
+        ParkingOrder parkingOrder = new ParkingOrder(null, 1, "2020-08-10", "2020-8-11", null, 1, null, StatusContants.WAIT_FOR_SURE, "浙A1063警", 10.0);
 
         //when
-        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,illegalPhone, email));
+        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder, illegalPhone, email));
 
         //then
         assertEquals(IllegalParameterException.class, exception.getClass());
@@ -93,26 +108,27 @@ public class ParkingOrdersServiceTest {
     @Test
     void should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_car_number_123() {
         //given
-        String illegal_car_number="123";
-        String phone="15920138477";
-        String email="1214852999@qq.com";
-        ParkingOrder parkingOrder=new ParkingOrder(null,1,"2020-08-10","2020-8-11",null,1,null, StatusContants.WAIT_FOR_SURE,illegal_car_number,10.0);
+        String illegal_car_number = "123";
+        String phone = "15920138477";
+        String email = "1214852999@qq.com";
+        ParkingOrder parkingOrder = new ParkingOrder(null, 1, "2020-08-10", "2020-8-11", null, 1, null, StatusContants.WAIT_FOR_SURE, illegal_car_number, 10.0);
 
         //when
-        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,phone, email));
+        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder, phone, email));
 
         //then
         assertEquals(IllegalParameterException.class, exception.getClass());
     }
+
     @Test
     void should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_email_123() {
         //given
-        String illegalEmail="123";
-        String phone="15920138477";
-        ParkingOrder parkingOrder=new ParkingOrder(null,1,"2020-08-10","2020-8-11",null,1,null, StatusContants.WAIT_FOR_SURE,"浙A1063警",10.0);
+        String illegalEmail = "123";
+        String phone = "15920138477";
+        ParkingOrder parkingOrder = new ParkingOrder(null, 1, "2020-08-10", "2020-8-11", null, 1, null, StatusContants.WAIT_FOR_SURE, "浙A1063警", 10.0);
 
         //when
-        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder,phone,illegalEmail));
+        Exception exception = assertThrows(IllegalParameterException.class, () -> parkingOrderService.addParkingOrder(parkingOrder, phone, illegalEmail));
 
         //then
         assertEquals(IllegalParameterException.class, exception.getClass());
