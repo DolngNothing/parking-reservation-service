e27cf40fad5c60ad8427016f37a943323e5fdc37
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-10 14:30:10 +0800

    [ParkingOrder][James] refactor:rename the file of order to parkingorder

---
+++ src/main/java/com/oocl/parkingreservationservice/constants/StatusContants.java
@@ -0,0 +1,7 @@
+package com.oocl.parkingreservationservice.constants;
+
+public class StatusContants {
+    final static int ALREADY_SURE=1;
+    final static int DELETED=-1;
+    final static int WAIT_FOR_SURE=0;
+}
--- src/test/java/com/oocl/parkingreservationservice/service/ParkingOrdersServiceTest.java
+++ src/test/java/com/oocl/parkingreservationservice/service/ParkingOrdersServiceTest.java
@@ -7,7 +7,7 @@ public class ParkingOrdersServiceTest {
     void should_throw_illegal_parameter_exception_when_book_parking_lot_given_illegal_phone_number_123() {
         //given
         String illegalPhone="123";
-        ParkingOrder parkingOrder=new ParkingOrder(null,1,1,)
+        ParkingOrder parkingOrder=new ParkingOrder(null,1,1)
         //when
 
         //then
