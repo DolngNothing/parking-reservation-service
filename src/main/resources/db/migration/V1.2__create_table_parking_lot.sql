16adc3a10a3a8884408cfdce289349e21931032e
ZHOUGR2 <286348794@qq.com>
2020-08-10 13:09:25 +0800

    [init] [Green] feature: add flyaway and jpa to project

---
+++ src/main/resources/db/migration/V1.2__create_table_parking_lot.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `parking_lot`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `latitude` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `longtitude` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `price` double(10, 2) DEFAULT NULL,
+  `description` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `image_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
