738b76cd2e72baf9f41e19cb848e056ef204c8bc
alex <alex.h.z.xu@oocl.com>
2020-08-11 15:02:45 +0800

    [alex] refactor: comment file

--- src/test/java/com/oocl/parkingreservationservice/controller/ParkingLotIntegrationTest.java
+++ src/test/java/com/oocl/parkingreservationservice/controller/ParkingLotIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class ParkingLotIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
