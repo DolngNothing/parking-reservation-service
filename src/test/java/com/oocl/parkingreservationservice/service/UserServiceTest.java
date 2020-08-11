e7a16da1f28fe8c5ac65808e233bddc65af923e5
Siaoja <lxj971010>
2020-08-11 15:29:42 +0800

    [story014][spike] feature: add test method should_throw_wrong_password_exception_and_return_wrong_password_when_login_given_right_phone_wrong_password.

--- src/test/java/com/oocl/parkingreservationservice/service/UserServiceTest.java
+++ src/test/java/com/oocl/parkingreservationservice/service/UserServiceTest.java
@@ -1,12 +1,14 @@
 package com.oocl.parkingreservationservice.service;
 
 import com.oocl.parkingreservationservice.dto.UserLoginResponse;
+import com.oocl.parkingreservationservice.exception.IllegalParameterException;
 import com.oocl.parkingreservationservice.model.User;
 import com.oocl.parkingreservationservice.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -38,5 +40,23 @@ public class UserServiceTest {
         assertEquals(user.getPhoneNumber(), loginedUser.getPhoneNumber());
         assertEquals(user.getPassword(), loginedUser.getPassword());
         assertEquals(user.getEmail(), loginedUser.getEmail());
+    }
+
+
+    @Test
+    void should_throw_wrong_password_exception_and_return_wrong_password_when_login_given_right_phone_wrong_password() {
+        //given
+        String phone = "13427560238";
+        String password = "456";
+        User user = new User(1, "13427560238", "1610692147@qq.com", "spike", "123");
+        given(userRepository.findByPhone(phone)).willReturn(user);
+
+        //when
+        User loginedUser = userService.login(phone, password);
+        Exception exception = assertThrows(IllegalParameterException.class, () -> userService.login(phone,password));
+
+        //then
+        assertEquals(IllegalParameterException.class, exception.getClass());
+        assertEquals("密码不正确！", exception.getMessage());
     }
 }
