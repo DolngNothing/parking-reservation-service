bb943f984f4b5d9d26195882d2dd0e6c53c054eb
Siaoja <lxj971010>
2020-08-11 15:47:26 +0800

    [story014][spike] feature: add test method should_throw_not_exist_user_exception_and_return_not_exist_user_infomation_when_login_given_not_exist_phone_and_wrong_password.

--- src/main/java/com/oocl/parkingreservationservice/constants/MessageConstants.java
+++ src/main/java/com/oocl/parkingreservationservice/constants/MessageConstants.java
@@ -5,4 +5,6 @@ public class MessageConstants {
     public final static String ODER_CANCELED = "订单已取消，请重新预约";
     public final static String ODER_NOT_EXIST = "订单不存在，无法确认";
     public final static String WRONG_PASSWORD = "密码不正确";
+    public final static String USER_NOT_EXIST = "用户不存在";
+
 }
--- src/test/java/com/oocl/parkingreservationservice/service/UserServiceTest.java
+++ src/test/java/com/oocl/parkingreservationservice/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.oocl.parkingreservationservice.service;
 import com.oocl.parkingreservationservice.constants.MessageConstants;
 import com.oocl.parkingreservationservice.dto.UserLoginResponse;
 import com.oocl.parkingreservationservice.exception.IllegalParameterException;
+import com.oocl.parkingreservationservice.exception.UserNotExistException;
 import com.oocl.parkingreservationservice.model.User;
 import com.oocl.parkingreservationservice.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -58,5 +59,21 @@ public class UserServiceTest {
         //then
         assertEquals(IllegalParameterException.class, exception.getClass());
         assertEquals(MessageConstants.WRONG_PASSWORD, exception.getMessage());
+    }
+
+
+    @Test
+    void should_throw_not_exist_user_exception_and_return_not_exist_user_infomation_when_login_given_not_exist_phone_and_wrong_password() {
+        //given
+        String phone = "1342756023";
+        String password = "456";
+        given(userRepository.findByPhone(phone)).willReturn(null);
+
+        //when
+        Exception exception = assertThrows(UserNotExistException.class, () -> userService.login(phone, password));
+
+        //then
+        assertEquals(UserNotExistException.class, exception.getClass());
+        assertEquals(MessageConstants.USER_NOT_EXIST, exception.getMessage());
     }
 }
