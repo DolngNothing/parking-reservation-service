c416b8e9a653d273cb14269c14446b8ae00a0eaf
alex <alex.h.z.xu@oocl.com>
2020-08-12 21:13:53 +0800

    [alex] fix bug: fix 200 bug

--- src/test/java/com/oocl/parkingreservationservice/integration/CommentIntegrationTest.java
+++ src/test/java/com/oocl/parkingreservationservice/integration/CommentIntegrationTest.java
@@ -51,7 +51,7 @@ public class CommentIntegrationTest {
                 "}";
         //when
         mockMvc.perform(post(("/comments")).contentType(MediaType.APPLICATION_JSON).content(commentInfo))
-                .andExpect(status().isCreated())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").isNumber())
                 .andExpect(jsonPath("$.orderId").isNumber())
                 .andExpect(jsonPath("$.userId").isNumber())
