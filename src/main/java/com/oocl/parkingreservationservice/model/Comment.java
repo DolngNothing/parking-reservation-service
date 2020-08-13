679b038b85f9b3d37020890e7c28aa7e1dff7a71
alex <alex.h.z.xu@oocl.com>
2020-08-13 14:13:58 +0800

    [alex] feature: add param to comment

--- src/main/java/com/oocl/parkingreservationservice/model/Comment.java
+++ src/main/java/com/oocl/parkingreservationservice/model/Comment.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -22,6 +19,7 @@ public class Comment {
     private Integer userId;
     private Double score;
     private String content;
+    @Column(name = "username")
     private String userName;
     private String createTime;
 }
