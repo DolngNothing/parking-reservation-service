5d9c45f701326a62bc9ac4d42baeb563ec14e51b
DolngNothing <1178363215@qq.com>
2020-08-13 21:47:02 +0800

    [009][chritopher] refactor:refactor Websocket

--- src/main/java/com/oocl/parkingreservationservice/utils/WebsocketService.java
+++ src/main/java/com/oocl/parkingreservationservice/utils/WebsocketService.java
@@ -35,7 +35,7 @@ public class WebsocketService {
     public WebsocketService(ParkingLotRepository parkingLotRepository, ParkingOrderRepository parkingOrderRepository, SimpMessagingTemplate messagingTemplate) {
         this.parkingLotRepository = parkingLotRepository;
         this.parkingOrderRepository = parkingOrderRepository;
-        this.messagingTemplate=messagingTemplate;
+        this.messagingTemplate = messagingTemplate;
     }
 
     public void sendToAllConnet(String msg) throws ParseException {
@@ -44,7 +44,7 @@ public class WebsocketService {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         long orderStartTime = sdf.parse(arr[1]).getTime();
         long orderEndTime = sdf.parse(arr[2]).getTime();
-        msg="/"+orderStartTime+"/"+orderEndTime+"/"+arr[3];
+        msg = "/" + orderStartTime + "/" + orderEndTime + "/" + arr[3];
         String[] arr1 = msg.split("/");
 
         WebsocketRequest request = new WebsocketRequest(Long.parseLong(arr1[1]), Long.parseLong(arr1[2]), Integer.parseInt(arr1[3]));
@@ -54,7 +54,7 @@ public class WebsocketService {
         List<ParkingOrder> allByParkingLotId = parkingOrderRepository.findAllByParkingLotId(request.getParkingLotId());
         List<ParkingOrder> parkingOrders = allByParkingLotId.stream().filter(ParkingOrder::isSure).collect(Collectors.toList());
 
-        System.out.println("预约成功时看看对应订单数量："+parkingOrders.size());
+        System.out.println("预约成功时看看对应订单数量：" + parkingOrders.size());
         ParkingLot parkingLot = parkingLotRepository.findById(request.getParkingLotId()).orElse(null);
         //遍历寻找广播:
         Set<Map.Entry<WebsocketRequest, String>> set = hashMap.entrySet();
@@ -62,7 +62,7 @@ public class WebsocketService {
             if (isTimeSame(e, request)) {
                 assert parkingLot != null;
                 Integer count = count(e.getKey(), parkingOrders, parkingLot.getCapacity());
-                System.out.println("save send: "+ e.getValue()+"count: "+count);
+                System.out.println("save send: " + e.getValue() + "count: " + count);
                 messagingTemplate.convertAndSend("/topic" + e.getValue(), count);
             }
         }
@@ -79,11 +79,10 @@ public class WebsocketService {
         List<ParkingOrder> allByParkingLotId = parkingOrderRepository.findAllByParkingLotId(request.getParkingLotId());
         List<ParkingOrder> parkingOrders = allByParkingLotId.stream().filter(ParkingOrder::isSure).collect(Collectors.toList());
         ParkingLot parkingLot = parkingLotRepository.findById(request.getParkingLotId()).orElse(null);
-        System.out.println("订阅成功时看看对应订单数量："+parkingOrders.size());
         assert parkingLot != null;
         Integer count = count(request, parkingOrders, parkingLot.getCapacity());
-        System.out.println("register send: "+ msg+"count: "+count);
-        messagingTemplate.convertAndSend("/topic" + msg,count);
+        System.out.println("register send: " + msg + "count: " + count);
+        messagingTemplate.convertAndSend("/topic" + msg, count);
     }
 
     public Boolean isTimeSame(Map.Entry<WebsocketRequest, String> e, WebsocketRequest request) {
@@ -97,14 +96,17 @@ public class WebsocketService {
     public Integer count(WebsocketRequest websocketRequest, List<ParkingOrder> allByParkingLotId, Integer capacity) throws ParseException {
         Integer count = 0;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        for (ParkingOrder parkingOrder : allByParkingLotId) {
-            long orderStartTime = sdf.parse(parkingOrder.getParkingStartTime()).getTime();
-            long orderEndTime = sdf.parse(parkingOrder.getParkingEndTime()).getTime();
-            if (websocketRequest.getEndTime() < orderStartTime || websocketRequest.getStartTime() > orderEndTime) {
-                continue;
+        if (allByParkingLotId != null && allByParkingLotId.size() > 0) {
+            for (ParkingOrder parkingOrder : allByParkingLotId) {
+                long orderStartTime = sdf.parse(parkingOrder.getParkingStartTime()).getTime();
+                long orderEndTime = sdf.parse(parkingOrder.getParkingEndTime()).getTime();
+                if (websocketRequest.getEndTime() < orderStartTime || websocketRequest.getStartTime() > orderEndTime) {
+                    continue;
+                }
+                count++;
             }
-            count++;
+            return capacity - count;
         }
-        return capacity - count;
+        return capacity;
     }
 }
